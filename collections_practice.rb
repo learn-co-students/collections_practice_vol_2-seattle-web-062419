967a71ef0e3224ff59d389c5f74c00162c043e4c
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-16 19:18:50 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
 # your code goes here
+def begins_with_r(array)
+  array.each do |element|
+    if !element.start_with?("r")
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  elements_w_a = []
+  array.each do |element|
+    if element.include?("a")
+      elements_w_a << element
+    end
+  end
+  elements_w_a
+end
+
+def first_wa(array)
+  array.each do |element|
+    if element[0, 2] == "wa"
+      return element
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.each do |element|
+    if element.is_a?(Symbol)
+      array.delete(element)
+    end
+  end
+  array
+end
