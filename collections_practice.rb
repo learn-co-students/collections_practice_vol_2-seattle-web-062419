9c6757911245157eacd27c8245287fb7655c8360
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-17 04:30:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,6 @@
 # your code goes here
 def begins_with_r(array)
-  array.all? { |element| element.start_with?("r")}
+  array.all? {|element| element.start_with?("r")}
 end
 
 def contain_a(array)
@@ -8,9 +8,23 @@ def contain_a(array)
 end
 
 def first_wa(array)
-  array.find { |element| element[0, 2] == "wa"}
+  array.find {|element| element[0, 2] == "wa"}
 end
 
 def remove_non_strings(array)
-  array.reject! { |element| !element.is_a?(String)}
+  array.reject! {|element| !element.is_a?(String)}
+end
+
+def count_elements(array)
+  count_array = []
+  array.uniq.each do |element|
+    if !count_array.include?(element)
+      count_array << {:count=> array.count(element), :name=> element[:name]}
+    end
+  end
+  count_array
+end
+
+def merge_data(data1, data2)
+  data1.push(data2.to_s).flatten
 end
